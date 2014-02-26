6f40e2df42105dabe9c045799bf0af8c40ed17a6
dsousa <dsousa@student.42.fr>
2014-02-26 14:45:12 +0100

    main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,24 @@
 /*   By: dsousa <dsousa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/26 13:19:14 by rbenjami          #+#    #+#             */
-/*   Updated: 2014/02/26 14:38:58 by dsousa           ###   ########.fr       */
+/*   Updated: 2014/02/26 14:44:54 by dsousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <libft.h>
+#include "sh.h"
+
+int		main(void)
+{
+	char	*line;
+
+	while (1)
+	{
+		ft_putstr("~> ");
+		get_next_line(1, &line);
+		if (line[0] != '\0')
+			ft_putendl(line);
+		free(line);
+	}
+	return (0);
+}
