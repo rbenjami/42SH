af4b56a23571e6d8c489b1c7b14b93f08d103cb7
dsousa <dsousa@student.42.fr>
2014-02-26 14:39:56 +0100

    test

--- srcs/main.c
+++ srcs/main.c
@@ -3,27 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbenjami <rbenjami@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dsousa <dsousa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/26 13:19:14 by rbenjami          #+#    #+#             */
-/*   Updated: 2014/02/26 13:36:41 by rbenjami         ###   ########.fr       */
+/*   Updated: 2014/02/26 14:38:58 by dsousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
-#include "sh.h"
-
-int		main(void)
-{
-	char	*line;
-
-	while (1)
-	{
-		ft_putstr("~> ");
-		get_next_line(1, &line);
-		if (line[0] != '\0')
-			ft_putendl(line);
-		free(line);
-	}
-	return (0);
-}
