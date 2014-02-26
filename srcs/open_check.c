5ebcf559ca7a99ebffaaee326324555dfed401b2
dsousa <dsousa@student.42.fr>
2014-02-26 15:09:11 +0100

    main.c
    Main a ne plus toucher avant parsing

--- srcs/open_check.c
+++ srcs/open_check.c
@@ -1,34 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   open_check.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dsousa <dsousa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/02/26 13:19:14 by rbenjami          #+#    #+#             */
-/*   Updated: 2014/02/26 15:05:32 by dsousa           ###   ########.fr       */
+/*   Created: 2014/02/26 15:01:26 by dsousa            #+#    #+#             */
+/*   Updated: 2014/02/26 15:06:42 by dsousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 #include "sh.h"
 
-void	parse_line(char *line)
+void	open_check(char *path)
 {
-	open_check(line);
-}
-
-int		main(void)
-{
-	char	*line;
-
-	while (1)
-	{
-		ft_putstr("~> ");
-		get_next_line(1, &line);
-		if (line[0] != '\0')
-			parse_line(line);
-		free(line);
-	}
-	return (0);
+	struct stat		stat;
 }
