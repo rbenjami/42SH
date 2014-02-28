f3eac0d763741d7cdeb1925ca9c2c8116a9a6650
Mgarcin <froh@free.fr>
2014-02-28 15:48:24 +0100

    Ast un peu plus avance
    Time to check tokens

--- srcs/ast.c
+++ srcs/ast.c
@@ -6,7 +6,7 @@
 /*   By: mgarcin <mgarcin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/28 14:50:14 by mgarcin           #+#    #+#             */
-/*   Updated: 2014/02/28 15:02:00 by mgarcin          ###   ########.fr       */
+/*   Updated: 2014/02/28 15:48:04 by mgarcin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,57 @@ typedef struct		s_token
 	struct s_token	*next;
 }					t_token;
 */
-void	ft_swap_node_right(t_ctrl)
-{
 
+int		ft_create_tree(t_ctrl *ctrl);
+int		ft_swap_node_right(t_ast **n_to_swap);
+int		ft_swap_node_left(t_ast **n_to_swap);
+int		ft_insert_left(t_ast **n_node, t_ast **n_to_ins);
+int		ft_insert_right(t_ast **n_node, t_ast **n_to_ins);
+
+
+int		ft_create_tree(t_ctrl *ctrl) // A GENERER SELON TOKENS
+{
+	if (!ctrl->tk_start)
+		return (-1);
+	return (0);
 }
 
-void	ft_swap_node_left()
+int		ft_swap_node_right(t_ast **n_to_swap)
 {
+	t_ast	*tmp;
 
+	tmp = *n_to_swap;
+	if (!(*n_to_swap)->right)
+		return (-1);
+	*n_to_swap = *(n_to_swap)->right;
+	*(n_to_swap)->right = tmp;
+	return (0);
+}
+
+int		ft_swap_node_left(t_ast **n_to_swap)
+{
+	t_ast	*tmp;
+
+	tmp = *n_to_swap;
+	if (!(*n_to_swap)->right)
+		return (-1);
+	*n_to_swap = *(n_to_swap)->left;
+	*(n_to_swap)->left = tmp;
+	return (0);
+}
+
+int		ft_insert_left(t_ast **n_node, t_ast **n_to_ins)
+{
+	if (!(n_node) || !(*n_to_ins))
+		return (-1);
+	(*n_node)->left = *n_to_ins;
+	return (0);
+}
+
+int		ft_insert_right(t_ast **n_node, t_ast **n_to_ins)
+{
+	if (!(n_node) || !(*n_to_ins))
+		return (-1);
+	(*n_node)->right = *n_to_ins;
+	return (0);
 }
