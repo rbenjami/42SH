fad7140e82007744a6a515109de9c21eb147dcce
Mgarcin <froh@free.fr>
2014-02-28 15:04:24 +0100

    Ast
    Ast.c

---
+++ srcs/ast.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ast.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgarcin <mgarcin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/02/28 14:50:14 by mgarcin           #+#    #+#             */
+/*   Updated: 2014/02/28 15:02:00 by mgarcin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+typedef struct 		s_ast
+{
+	t_token			*tk;
+	struct s_ast	*left;
+	struct s_ast	*right;
+}					t_ast;
+
+typedef struct		s_ctrl
+{
+	t_ast			*ast_start;
+	t_token			*tk_start;
+	char			**env;
+}					t_ctrl;
+
+typedef struct		s_token
+{
+	char			*value;
+	enum e_token	type;
+	struct s_token	*next;
+}					t_token;
+*/
+void	ft_swap_node_right(t_ctrl)
+{
+
+}
+
+void	ft_swap_node_left()
+{
+
+}
