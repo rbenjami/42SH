fb51467c786317fc700a0ddbb12a32714b62c4ad
dsousa <dsousa@student.42.fr>
2014-03-11 17:01:09 +0100

    *echo
    Finalisation du echo (stable)
    Légère corrections a venir.

--- srcs/builtin/echo.c
+++ srcs/builtin/echo.c
@@ -6,67 +6,67 @@
 /*   By: dsousa <dsousa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/10 18:13:04 by dsousa            #+#    #+#             */
-/*   Updated: 2014/03/11 15:23:09 by dsousa           ###   ########.fr       */
+/*   Updated: 2014/03/11 16:59:35 by dsousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sh.h"
 
-static char		*create_var(char *str, int start)
+static char		*create_var(char *str)
 {
 	int		i;
-	char	*tmp;
 
-	i = start;
+	i = 0;
 	while (str[i] && !ft_isspace(str[i]) && str[i] != '$')
 		i++;
-	tmp = ft_strsub(str, start, i - start);
+	str[i] = '\0';
 	return (str);
 }
 
-static void		smart_print(char *str)
+static int		smart_print(char *str)
 {
+	int		wr;
 	int		i;
 	char	*var;
 
 	i = 0;
+	wr = 0;
+	var = NULL;
 	while (str[i] && str[i] != '$')
 	{
+		wr++;
 		write(1, &str[i], 1);
 		i++;
 	}
-	if (str[i] && str[i + 1] && !(var = create_var(str, i)))
-	{
-		write(1, &str[i], 1);
-		smart_print(&str[i + 1]);
-	}
-	else if (str[i] && (var = ft_getenv(var)) == NULL)
-	{
-		write(1, &str[i], 1);
-		smart_print(&str[i + 1]);
-	}
-	else if (str[i] && var)
+	var = str[i + 1] ? ft_getenv(create_var(&str[i + 1])) : NULL;
+	wr = var ? wr + 1 : wr;
+	if (var)
 		ft_putstr(var);
+		if (str[i + 1])
+		i++;
 	while (str[i] && !ft_isspace(str[i]) && str[i] != '$')
 		i++;
 	if (str[i])
+		write(1, &str[i++], 1);
+	if (str[i])
 		smart_print(&str[i]);
+	return (wr);
 }
 
 int				builtin_echo(char **argv)
 {
 	int		i;
 	int		dollar;
+	int		wr;
 
-	i = 1;
 	dollar = 0;
+	wr = 0;
 	if (argv[1])
 	{
-		if (ft_strcmp(argv[1], "-n") == 0)
-			i = 2;
+		i = (ft_strcmp(argv[1], "-n") == 0) ? 2 : 1;
 		while (argv[i])
 		{
-			smart_print(argv[i]);
+			wr += smart_print(argv[i]);
 			if (argv[i + 1])
 				write(1, " ", 1);
 			i++;
@@ -75,7 +75,7 @@ int				builtin_echo(char **argv)
 			ft_putstr("\033[7m%\033[m");
 		write(1, "\n", 1);
 	}
-	else
+	if (!argv[1] || wr == 0)
 		write(1, "\n", 1);
 	exit(0);
 	return (-1);
