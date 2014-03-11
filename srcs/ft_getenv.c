5274dc815898bdd70805d4bd34ba54992ddfbf88
ZionLivity <benjigo18@gmail.com>
2014-03-11 14:40:24 +0100

    ~ fuite ft_getenv

--- srcs/ft_getenv.c
+++ srcs/ft_getenv.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_getenv.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsousa <dsousa@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rbenjami <rbenjami@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/10 18:14:38 by dsousa            #+#    #+#             */
-/*   Updated: 2014/03/10 18:14:52 by dsousa           ###   ########.fr       */
+/*   Updated: 2014/03/11 14:33:41 by rbenjami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,24 @@
 char	*ft_getenv(const char *name)
 {
 	int				i;
+	int				lenth;
+	char			*tmp;
 
 	i = 0;
-	name = ft_strjoin(name, "=");
-	while (environ[i] && ft_strncmp(environ[i], name, ft_strlen(name)))
+	tmp = ft_strjoin(name, "=");
+	while (environ[i] && ft_strncmp(environ[i], tmp, ft_strlen(tmp)))
 		i++;
 	if (environ[i])
-		return (&environ[i][ft_strlen(name)]);
+	{
+		lenth = ft_strlen(tmp);
+		ft_strdel(&tmp);
+		return (&environ[i][lenth]);
+	}
 	else
 	{
-		write(2, name, (ft_strlen(name) - 1));
+		lenth = ft_strlen(tmp);
+		write(2, tmp, (lenth - 1));
+		ft_strdel(&tmp);
 		ft_putendl_fd(": not found", 2);
 		return (NULL);
 	}
