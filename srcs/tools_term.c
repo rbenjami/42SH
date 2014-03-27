906dcf6f086938236e6937cd566a063b30d186f5
dsousa <dsousa@student.42.fr>
2014-03-27 12:36:09 +0100

    ~ len_prompt après unsetenv

--- srcs/tools_term.c
+++ srcs/tools_term.c
@@ -6,7 +6,7 @@
 /*   By: dsousa <dsousa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/13 19:11:20 by dsousa            #+#    #+#             */
-/*   Updated: 2014/03/27 11:56:19 by dsousa           ###   ########.fr       */
+/*   Updated: 2014/03/27 12:34:40 by dsousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,15 @@ int			len_prompt(void)
 		return (handler.len);
 	i = 0;
 	len = 4;
-	if (!(pwd = ft_getenv("PWD")) || !(home = ft_getenv("HOME")))
-		return (55);
-	if (!ft_strncmp(pwd, home, ft_strlen(home)))
+	home = ft_getenv("HOME");
+	pwd = ft_getenv("PWD");
+	if (pwd && home && !ft_strncmp(pwd, home, ft_strlen(home)))
 	{
 		len++;
 		i = ft_strlen(home);
 	}
 	len += ft_strlen(ln = ft_getenv("LOGNAME"));
-	len += ft_strlen(pwd + i);
+	len = pwd ? len + ft_strlen(pwd + i) : len;
 	if (handler.cmd != 0)
 		len += ft_strlen(ft_itoa(handler.cmd)) + 1;
 	ft_strdel(&pwd);
